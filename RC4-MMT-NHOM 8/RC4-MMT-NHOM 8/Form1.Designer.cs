ce27ead7489e94fe35ce046b57226345d65197dd
anhkhoi-k46 <2212396@dlu.edu.vn>
2025-04-25 18:51:15 +0700

    Add files via upload

---
+++ RC4-MMT-NHOM 8/RC4-MMT-NHOM 8/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace RC4_MMT_NHOM_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPlaintext = new System.Windows.Forms.TextBox();
+            this.txtCyberText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(170, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Chuyển đổi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(93, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BỘ CHUYỂN ĐỔI MÃ HOÁ RC4";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtPlaintext
+            // 
+            this.txtPlaintext.Location = new System.Drawing.Point(33, 71);
+            this.txtPlaintext.Multiline = true;
+            this.txtPlaintext.Name = "txtPlaintext";
+            this.txtPlaintext.Size = new System.Drawing.Size(217, 54);
+            this.txtPlaintext.TabIndex = 2;
+            this.txtPlaintext.TextChanged += new System.EventHandler(this.txtPlaintext_TextChanged);
+            // 
+            // txtCyberText
+            // 
+            this.txtCyberText.Location = new System.Drawing.Point(33, 206);
+            this.txtCyberText.Multiline = true;
+            this.txtCyberText.Name = "txtCyberText";
+            this.txtCyberText.Size = new System.Drawing.Size(217, 72);
+            this.txtCyberText.TabIndex = 2;
+            this.txtCyberText.TextChanged += new System.EventHandler(this.txtCybertext_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Plaintext ";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "CyberText";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(276, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 22);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Key";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(280, 71);
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(198, 54);
+            this.txtKey.TabIndex = 2;
+            this.txtKey.TextChanged += new System.EventHandler(this.txtPlaintext_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(490, 315);
+            this.Controls.Add(this.txtCyberText);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtPlaintext);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPlaintext;
+        private System.Windows.Forms.TextBox txtCyberText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKey;
+    }
+}
+
---
+++ RC4-MMT-NHOM 8/RC4-MMT-NHOM 8/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RC4_MMT_NHOM_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private byte[] RC4(byte[] data, byte[] key)
+        {
+            byte[] S = new byte[256];
+            for (int i = 0; i < 256; i++)
+                S[i] = (byte)i;
+
+            int j = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                j = (j + S[i] + key[i % key.Length]) % 256;
+                Swap(S, i, j);
+            }
+
+            byte[] result = new byte[data.Length];
+            int iIndex = 0, jIndex = 0;
+
+            for (int k = 0; k < data.Length; k++)
+            {
+                iIndex = (iIndex + 1) % 256;
+                jIndex = (jIndex + S[iIndex]) % 256;
+
+                Swap(S, iIndex, jIndex);
+
+                int t = (S[iIndex] + S[jIndex]) % 256;
+                byte keystreamByte = S[t];
+
+                result[k] = (byte)(data[k] ^ keystreamByte);
+            }
+
+            return result;
+        }
+
+        private void Swap(byte[] array, int i, int j)
+        {
+            byte temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string plaintext = txtPlaintext.Text;
+            string key = txtKey.Text;
+
+            if (string.IsNullOrEmpty(plaintext) || string.IsNullOrEmpty(key))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ Plaintext và Key!");
+                return;
+            }
+
+            byte[] plaintextBytes = Encoding.ASCII.GetBytes(plaintext);
+            byte[] keyBytes = Encoding.ASCII.GetBytes(key);
+            byte[] resultBytes = RC4(plaintextBytes, keyBytes);
+
+            // Hiển thị kết quả dưới dạng Hex cho dễ nhìn
+            txtCyberText.Text = BitConverter.ToString(resultBytes).Replace("-", " ");
+        }
+       
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCybertext_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPlaintext_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
